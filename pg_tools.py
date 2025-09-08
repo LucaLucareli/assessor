14b8e0871a4585a16ab002edf25f9bc058b07c99
LucaLucareli <lucalucareli@gmail.com>
2025-09-08 08:46:43 -0300

    Fix: Now llm will be able to insert the category in the table

--- pg_tools.py
+++ pg_tools.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, Field
 from langchain.tools import tool
 from dotenv import load_dotenv
 from typing import Optional
+import unicodedata
 import psycopg2
 import os
 
@@ -13,17 +14,45 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 def get_conn():
     return psycopg2.connect(DATABASE_URL)
 
+TYPE_LIST = {
+    "INCOME": "INCOME", "ENTRADA": "INCOME", "RECEITA": "INCOME", "SÁLARIO": "INCOME",
+    "EXPENSE": "EXPENSES", "EXPENSES": "EXPENSES", "DESPESA": "EXPENSES", "GASTO": "EXPENSES",
+    "TRANSFER": "TRANSFER",  "TRANSFERÊNCIA": "TRANSFER", "TRANSFERENCIA": "TRANSFER"
+}
+
 def _resolve_type_id(cur, type_id: Optional[int], type_name: Optional[str]) -> Optional[int]:
     if type_name:
         t = type_name.strip().upper()
-        if t == "EXPENSE":
-            t = "EXPENSES"
+        if t in TYPE_LIST:
+            t = TYPE_LIST[t]
         cur.execute("SELECT id FROM transaction_types WHERE UPPER(type)=%s LIMIT 1;", (t,))
         row = cur.fetchone()
         return row[0] if row else None
     if type_id:
         return int(type_id)
     return 2
+
+
+def normalize(text: str) -> str:
+    text = text.upper()
+    text = ''.join(c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn')
+    return text
+
+def _resolve_category_id(cur, category_id: Optional[int] = None, category_name: Optional[str] = None) -> Optional[int]:
+    if category_id:
+        return category_id
+
+    if category_name:
+        cur.execute("SELECT id, name FROM categories")
+        rows = cur.fetchall() 
+        CATEGORY_MAP = {normalize(name): id_ for id_, name in rows}
+
+        key = normalize(category_name.strip())
+        mapped_id = CATEGORY_MAP.get(key)
+        if mapped_id:
+            return mapped_id
+
+    return None
 
 
 class AddTransactionArgs(BaseModel):
@@ -33,9 +62,10 @@ class AddTransactionArgs(BaseModel):
         default=None,
         description="Timestamp ISO 8601; se ausente, usa NOW() no banco."
     )
-    type_id: Optional[int] = Field(default=None, description="ID em transaction_types (1=INCOME, 2=EXPENSES, 3=TRANSFER).")
-    type_name: Optional[str] = Field(default=None, description="Nome do tipo: INCOME | EXPENSES | TRANSFER.")
-    category_id: Optional[int] = Field(default=None, description="FK de categories (opcional).")
+    type_id: Optional[int] = Field(default=None, description="ID em transaction_types (opcional) (1=INCOME, 2=EXPENSES, 3=TRANSFER).")
+    type_name: Optional[str] = Field(default=None, description="Nome do tipo (opcional) : INCOME | EXPENSES | TRANSFER.")
+    category_id: Optional[int] = Field(default=None, description="FK de categories (opcional):  (1=comida, 2=besteira, 3=estudo, 4=férias, 5=transporte, 6=moradia, 7=saúde, 8=lazer, 9=contas, 10=investimento, 11=presente, 12=outros)")
+    category_name: Optional[str] = Field(default=None, description="Nome da categories (opcional) : (comida, besteira, estudo, férias, transporte, moradia, saúde, lazer, contas, investimento, presente, outros)")
     description: Optional[str] = Field(default=None, description="Descrição (opcional).")
     payment_method: Optional[str] = Field(default=None, description="Forma de pagamento (opcional).")
 
@@ -59,6 +89,7 @@ def add_transaction(
     type_id: Optional[int] = None,
     type_name: Optional[str] = None,
     category_id: Optional[int] = None,
+    category_name: Optional[str] = None,
     description: Optional[str] = None,
     payment_method: Optional[str] = None,
 ) -> dict:
@@ -69,6 +100,8 @@ def add_transaction(
         resolved_type_id = _resolve_type_id(cur, type_id, type_name)
         if not resolved_type_id:
             return {"status": "error", "message": "Tipo inválido (use type_id ou type_name: INCOME/EXPENSES/TRANSFER)."}
+        
+        resolve_category_id = _resolve_category_id(cur, category_id, category_name)
 
         if occurred_at:
             cur.execute(
@@ -79,7 +112,7 @@ def add_transaction(
                     (%s, %s, %s, %s, %s, %s::timestamptz, %s)
                 RETURNING id, occurred_at;
                 """,
-                (amount, resolved_type_id, category_id, description, payment_method, occurred_at, source_text),
+                (amount, resolved_type_id, resolve_category_id, description, payment_method, occurred_at, source_text),
             )
         else:
             cur.execute(
